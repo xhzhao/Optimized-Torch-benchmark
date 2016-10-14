1f8d083f52cabbc8e284638f32f694e64c012286
Xiaohui Zhao <xiaohui.zhao@intel.com>
2016-10-14 10:15:22 +0800

    update for alexnet cudnn

--- alexnet_cudnn.lua
+++ alexnet_cudnn.lua
@@ -1,42 +1,32 @@
 function alexnet(lib)
 
-   local SpatialConvolution = lib[1]
-   local SpatialMaxPooling = lib[2]
-   local ReLU = lib[3]
+   local features = nn.Sequential()
+
+   local SpatialConvolution = cudnn.SpatialConvolution
+   local ReLU = cudnn.ReLU
+   local SpatialMaxPooling = cudnn.SpatialMaxPooling
    local SBatchNorm = cudnn.SpatialBatchNormalization
+   local LRN = cudnn.SpatialCrossMapLRN
+
+   features:add(SpatialConvolution(3,96,11,11,4,4))       -- 224 -> 55
+   features:add(ReLU(true))
+   features:add(LRN(5,0.0001,0.75))
+   features:add(SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
+   features:add(SpatialConvolution(96,256,5,5,1,1,2,2,2))       --  27 -> 27
+   features:add(ReLU(true))
+   features:add(LRN(5,0.0001,0.75))
+   features:add(SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
+   features:add(SpatialConvolution(256,384,3,3,1,1,1,1))      --  13 ->  13
+   features:add(ReLU(true))
+   features:add(SpatialConvolution(384,384,3,3,1,1,1,1,2))      --  13 ->  13
+   features:add(ReLU(true))
+   features:add(SpatialConvolution(384,256,3,3,1,1,1,1,2))      --  13 ->  13
+   features:add(ReLU(true))
+   features:add(SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
 
 
-   local features = nn.Concat(2)
-   local fb1 = nn.Sequential() -- branch 1
-   --fb1:add(SpatialConvolution(3,48,11,11,4,4,2,2))       -- 224 -> 55
-   fb1:add(SpatialConvolution(3,48,11,11,4,4,0,0))       -- 227 -> 55
-   fb1:add(ReLU(true))
-   fb1:add(SBatchNorm(48))
-   fb1:add(SpatialMaxPooling(3,3,2,2))                   -- 55 ->  27
-   fb1:add(SpatialConvolution(48,128,5,5,1,1,2,2))       --  27 -> 27
-   fb1:add(ReLU(true))
-   fb1:add(SBatchNorm(128))
-   fb1:add(SpatialMaxPooling(3,3,2,2))                   --  27 ->  13
-   fb1:add(SpatialConvolution(128,192,3,3,1,1,1,1))      --  13 ->  13
-   fb1:add(ReLU(true))
-   fb1:add(SpatialConvolution(192,192,3,3,1,1,1,1))      --  13 ->  13
-   fb1:add(ReLU(true))
-   fb1:add(SpatialConvolution(192,128,3,3,1,1,1,1))      --  13 ->  13
-   fb1:add(ReLU(true))
-   fb1:add(SpatialMaxPooling(3,3,2,2))                   -- 13 -> 6
-   fb1:cuda()
-  
+   features:get(1).gradInput = nil
 
-
-   local fb2 = fb1:clone() -- branch 2
-   for k,v in ipairs(fb2:findModules('nn.SpatialConvolutionMKLDNN')) do
-      v:reset() -- reset branch 2's weights
-   end
-
-   --fb1:get(1).gradInput = nil
-   --fb2:get(1).gradInput = nil
-   features:add(fb1)
-   features:add(fb2)
    --features = makeDataParallel(features, nGPU) -- defined in util.lua
 
    -- 1.3. Create Classifier (fully connected layers)
@@ -49,7 +39,7 @@ function alexnet(lib)
    classifier:add(nn.Linear(4096, 4096))
    classifier:add(nn.Threshold(0, 1e-6))
    classifier:add(nn.Linear(4096, 1000))
-   --classifier:add(nn.LogSoftMax())
+   classifier:add(nn.LogSoftMax())
    --classifier:cuda()
 
 
